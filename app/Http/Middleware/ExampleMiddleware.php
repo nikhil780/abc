606755dfd34e323ecfd01b4e2da1e63c56f837b2
nikhil780 <nikhilsatpute2798@gmail.com>
2021-05-17 11:40:29 +0530

    s

--- app/Http/Middleware/ExampleMiddleware.php
+++ app/Http/Middleware/ExampleMiddleware.php
@@ -7,14 +7,5 @@ use Closure;
 class ExampleMiddleware
 {
     /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        return $next($request);
-    }
+ 
 }
